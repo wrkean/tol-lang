fc93e441e09c779a5703126d083a9b157f22632e
wrkean <barracaadrianewew@gmail.com>
2026-07-30 01:03:20 +0800

    docs(parser/expr): add more doc comments

--- src/parse/ast/expr.rs
+++ src/parse/ast/expr.rs
@@ -8,12 +8,21 @@ use crate::{
 
 /// Ast node representing expressions
 pub struct Expr {
+    /// Span pointing to the source this expression is parsed from. The node itself DOES NOT know
+    /// which source it points to, it is handled by the compilation pipeline
     span: Span,
+
+    /// The kind of expression
     kind: ExprKind,
+
+    /// Resolved variable, available only after name resolution. Some expressions may not have a
+    /// resolved var, typically those expressions that refer to no names like the integer
+    /// expression
     resolved_var: Option<ResolvedVar>,
 }
 
 impl Expr {
+    /// Creates a new expression
     pub fn new(span: Span, kind: ExprKind) -> Self {
         Self {
             span,
@@ -44,6 +53,8 @@ impl Expr {
             .expect("this expression's symbol id is never set")
     }
 
+    /// L-values are expressions that appear to the left of `=` in an assignment expression or
+    /// name declarations. This function returns true if this expression is indeed an l-value
     pub fn is_lvalue(&self) -> bool {
         use ExprKind::*;
         matches!(
@@ -53,44 +64,81 @@ impl Expr {
     }
 }
 
-/// The kind of expression AST node. Should be owned by `Expr`
+/// Enum representing different kinds of expressions
 pub enum ExprKind {
+    /// Integer literals
+    ///
+    /// e.g.: `1`
     Integer(i64),
+
+    /// Float literals
+    ///
+    /// e.g.: `4.2`
     Float(f64),
+
+    /// Identifiers. Names in the source code that are not keywords basically.
     Identifier(String),
-    AnonymousFn {
-        params: ParamList,
-        body: Box<Expr>,
-    },
+
+    /// Anonymous function
+    ///
+    /// e.g.: `|param1| param1 + 1`
+    AnonymousFn { params: ParamList, body: Box<Expr> },
+
+    /// String literals
+    ///
+    /// e.g.: `"Hello World!"`
     Str {
         text: String,
         interned_id: Option<usize>,
     },
+
+    /// Binary expressions
+    ///
+    /// e.g.: `4 + 2`
     Binary {
         left: Box<Expr>,
         right: Box<Expr>,
         op: Token,
     },
-    Call {
-        left: Box<Expr>,
-        args: Vec<Expr>,
-    },
-    FieldAccess {
-        object: Box<Expr>,
-        field: Token,
-    },
+
+    // TODO: Unary expressions
+    // Unary {
+    //      operand: Box<Expr>,
+    //      op: Token,
+    // }
+    //
+    /// Function call expressions
+    ///
+    /// e.g.: `foo()`
+    Call { left: Box<Expr>, args: Vec<Expr> },
+
+    /// Field access expressions
+    ///
+    /// e.g: `class_inst1.foo`
+    FieldAccess { object: Box<Expr>, field: Token },
+
+    /// List literals
+    ///
+    /// e.g. `[0, 1, 2, 3]` or `@[expr1; 100]`
     List {
         elements: Vec<Expr>,
         init: Option<ListInit>,
     },
-    IndexAccess {
-        left: Box<Expr>,
-        index: Box<Expr>,
-    },
+
+    /// Index access expression
+    ///
+    /// e.g.: `list1[index]`
+    IndexAccess { left: Box<Expr>, index: Box<Expr> },
 }
 
+/// The list initializer. During parsing, if `@` is encountered before a string literal, the parser,
+/// instead of parsing a list of expressions, parses a list initializer syntax instead:
+/// `@[<expr>: <count>]`
 pub struct ListInit {
+    /// The expression to initialize
     pub expr: Box<Expr>,
+
+    /// The capacity, filled with the expression given
     pub init_capacity: Token,
 }
 
