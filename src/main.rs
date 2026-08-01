7ef0e06a47ae9735b02dd77887b26c078beb9802
wrkean <barracaadrianewew@gmail.com>
2026-08-01 17:36:48 +0800

    chore: change option_env! to var_os

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use std::path::PathBuf;
+use std::{env, path::PathBuf};
 
 use clap::Parser;
 
@@ -23,7 +23,7 @@ mod vm;
 fn main() {
     let args = Cli::parse();
 
-    let stdlib_path = option_env!("TOL_STDLIB");
+    let stdlib_path = env::var_os("TOL_STDLIB");
     let stdlib_path = match stdlib_path {
         Some(s) => PathBuf::from(s),
         None => {
